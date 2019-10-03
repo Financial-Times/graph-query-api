b2766b9586e78842cf830ebebdd39ce77cda8d64
Elitsa Pavlova <elitsa.pavlova@ft.com>
2019-10-03 12:15:02 +0300

    Add simple search in the neo client

--- neoClient.go
+++ neoClient.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/neo4j/neo4j-go-driver/neo4j"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/Financial-Times/go-logger"
+	"github.com/neo4j/neo4j-go-driver/neo4j"
+)
 
 type NeoClient struct{
 	driver neo4j.Driver
@@ -25,7 +31,44 @@ type SearchObject struct{
 }
 
 func (nc *NeoClient) Search(sObj *SearchObject) ([]string, error){
-	return nil, nil
+	var session neo4j.Session
+	var records []neo4j.Record
+	var err error
+
+	statement := `
+		MATCH (:Concept{uuid:"9ab8e36c-4b79-4e96-9aae-cc586b7d19c4"})-[:EQUIVALENT_TO]->(canon:Concept)
+		MATCH (canon)<-[:EQUIVALENT_TO]-(leaves:Concept)-[:HAS_BROADER*1..]->(implicit:Concept)<-[]-(c:Content)
+		WITH c, leaves, implicit
+		WHERE c.publishedDateEpoch>1569179507 AND c.publishedDateEpoch<1570043512 AND c-[:ABOUT]->(:Topic{prefLabel:"Agriculture"})
+		RETURN c.uuid as uuid
+		LIMIT(5)`
+	if session, err = nc.driver.Session(neo4j.AccessModeRead); err != nil {
+		return nil, err
+	}
+	defer session.Close()
+
+	records, err = neo4j.Collect(session.ReadTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+		return tx.Run(statement, map[string]interface{}{"contentUUID": "", "annotationLifecycle": ""})
+	}))
+
+	logger.Infof("Query returned following results: %v", records)
+
+	if len(records) < 1 {
+		return nil, errors.New("no results found")
+	}
+
+	var results []string
+	for _, record := range records {
+		fmt.Printf("%+v\n", record)
+		recordUUID, ok := record.Get("uuid")
+		if !ok {
+			logger.Error("not found uuid for record")
+		}
+
+		results = append(results, fmt.Sprintf("%v",recordUUID))
+	}
+
+	return results, nil
 }
 
 func (nc *NeoClient) Close()error{
