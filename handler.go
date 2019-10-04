6edcce1a4500913006f2a8ccaeec36ea957ed25a
Elitsa Pavlova <elitsa.pavlova@ft.com>
2019-10-04 14:32:55 +0300

    fixes

--- handler.go
+++ handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -10,14 +11,17 @@ import (
 type requestHandler struct {
 	client *NeoClient
 }
-
+type Th struct{
+	Text string `json:"text"`
+	Id string `json:"id"`
+}
 type payload struct {
 	Period *struct {
 		Start int64 `json:"startDate"`
 		End   int64 `json:"endDate"`
 	} `json:"period,omitempty"`
-	IsRelatedWith         []string `json:"isRelatedWith"`
-	IsDirectlyRelatedWith []string `json:"isDirectlyRelatedWith"`
+	IsRelatedWith         []Th `json:"isRelatedWith"`
+	IsDirectlyRelatedWith []Th `json:"isDirectlyRelatedWith"`
 }
 
 type responseBody struct {
@@ -52,31 +56,46 @@ func (handler *requestHandler) searchEndpoint(writer http.ResponseWriter, reques
 		writeError(writer, err)
 		return
 	}
-
-	writer.WriteHeader(http.StatusOK)
+	writer.Header().Set("Content-Type", "application/json")
+	//writer.WriteHeader(http.StatusOK)
+	//writer.Header().Set("Content-Type", "application/json")
 	writer.Write(body)
 }
 
 func payloadToSearchObject(data payload) *SearchObject {
+	fmt.Printf("%+v",data)
 	var (
 		start int64 = 1538582036 // last year this time
 		end   int64 = time.Now().Unix()
 	)
-	if data.Period != nil {
-		start = data.Period.Start
-		end = data.Period.End
+	//if data.Period != nil {
+	//	start = data.Period.Start
+	//	end = data.Period.End
+	//}
+	//start = data.Period.Start
+	//end = data.Period.End
+	s := []string{}
+	for _,v := range(data.IsRelatedWith){
+		s = append(s, v.Id)
+	}
+	s1 := []string{}
+	for _,v := range(data.IsDirectlyRelatedWith){
+		s1 = append(s1, v.Id)
 	}
 	return &SearchObject{
 		fromDate:              start,
 		toDate:                end,
-		isRelatedWith:         data.IsRelatedWith,
-		isDirectlyRelatedWith: data.IsDirectlyRelatedWith,
+		isRelatedWith:         s,
+		isDirectlyRelatedWith: s1,
 		limit:                 25,
 	}
 }
 
 func writeError(writer http.ResponseWriter, err error) {
 	writer.WriteHeader(http.StatusInternalServerError)
+	fmt.Println("--------")
+	fmt.Println(err)
+	fmt.Println("--------")
 	body, _ := json.Marshal(responseBody{Error: err.Error()})
 	writer.Write(body)
 }
